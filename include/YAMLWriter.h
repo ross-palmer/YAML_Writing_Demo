363de98ce2ea0dada4309ecb71239402d17f4453
ross-palmer <ross.palmer@itcarlow.ie>
2019-03-27 09:22:55 +0000

    Basic demonstration of YAML writing

---
+++ include/YAMLWriter.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "yaml-cpp\yaml.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class YAMLWriter
+{
+public:
+	static void write();
+
+private:
+};
+
---
+++ src/YAMLWriter.cpp
@@ -0,0 +1,40 @@
+#include "YAMLWriter.h"
+
+void YAMLWriter::write()
+{
+	// Some high score data...
+	std::string names[] = { "Jim", "John", "Mary", "Zoe"};
+	int scores[] = { 1,2,3,4 };
+
+	// A YAML::Emitter acts as a YAML output stream 	
+	YAML::Emitter out;
+	out << YAML::BeginMap;
+	// Highscores is the top level mapping
+	// (it maps to a sequence as shown below)
+	out << YAML::Key << "Highscores";
+	out << YAML::Value << YAML::BeginSeq;
+	
+	for (int i = 0; i < 3; i++)
+	{
+		// Write out the sequence...
+		// Each entry in the sequence is a mapping of 
+	    //  name: playersname 	    
+		//  score: playerscore
+		out << YAML::BeginMap;
+		out << YAML::Key << "name";
+		out << YAML::Value << names[i];
+		out << YAML::Key << "score";
+		out << YAML::Value << std::to_string(scores[i]);
+		out << YAML::EndMap;
+	}
+	out << YAML::EndSeq;
+	out << YAML::EndMap;
+	std::cout << "Here's the output YAML:\n" << out.c_str();
+	
+	// Now write it to a file..
+	std::string file = ".\\Highscores.yaml";
+	std::ofstream fout(file); // This is the path to the YAML file
+
+	fout << out.c_str() << std::endl;
+	fout.close();
+}
